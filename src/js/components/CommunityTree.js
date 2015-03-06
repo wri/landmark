a34aade6c9711fdfe4c1e034530e39f4f48877ff
Lucas Cotner <lcotner@blueraster.com>
2015-03-06 13:40:46 -0500

    Updated pop-ups for new national level data, ui with Jeffs comps

--- src/js/components/CommunityTree.js
+++ src/js/components/CommunityTree.js
@@ -32,14 +32,14 @@ define([
       if (this.state.data[0].children) {
         for (var i = 0; i < this.state.data[0].children.length; i++) {
           if (this.state.data[0].children[i].id === key) {
-            activeKeys.push(key);
+            activeKeys.push(key); //TODO: or push i? --> See how layers are put together
           }
         }
         this.state.data = this.state.data[0].children;
       } else {
         for (var i = 0; i < this.state.data.length; i++) {
           if (this.state.data[i].id === key) {
-            activeKeys.push(key);
+            activeKeys.push(i);
           }
         }
       }
@@ -50,7 +50,29 @@ define([
       // Update the Tree Component
       this.setState({ data: dataSource });
       // Update the Layer UI
-      //LayerController.updateVisibleLayers(activeKeys); //TODO When we get data
+
+      //TODO: Map the activeKey's name to the proper Attribute here, so I can use that variable in the layer's Pop-Up to show the correct attributes --> Require layercontroller in map controller? how to get the react state in the map controller when we are assigning the correct fields in the popup??
+      //debugger;
+      //Determine whether we are in the National Tree when updating data
+      var nationalLayer = this.getDOMNode().dataset.reactid;
+      brApp.currentLayer = evt.target['dataset'].key;
+      if (nationalLayer === '.1' || nationalLayer === '.2') {
+        switch (activeKeys[0]) {
+                    case ".1": //Community Land Tenure
+                      visibleLayers = [0];
+                      break;
+                    case ".2": //Indigenous Land Tenure
+                      visibleLayers = [1];
+                      break;
+                    case ".3": //Percent of Community and Indigenous
+                      keys[0] += 2; //--> To get around the two Land Tenure Security Layers
+                      visibleLayers = keys;
+                      break;
+                }
+
+
+      }
+      LayerController.updateVisibleLayers(activeKeys, nationalLayer); //TODO When we get data
     },
     
     render: function () {
--- src/js/map/esriMap/LayerController.js
+++ src/js/map/esriMap/LayerController.js
@@ -13,27 +13,44 @@ define([
          * @param {array} keys - Array of keys from the checkbox tree, these are mapped to layer numbers
          * 										- in the MapConfig.layerMapping (may add layer id's to config if needed).
          */
-        updateVisibleLayers: function(keys) {
+        updateVisibleLayers: function(keys, layer) {
             brApp.debug('LayerController >>> updateVisibleLayers');
             console.log(keys);
-            // Currently there is only one layer, as the app grows and more layers are added
-            // add some data to the MapConfig.layerMapping so we know which layer id to use and
-            // which sublayers to turn on
-            var dynamicLayer = brApp.map.getLayer('indigenousLands'),
-                visibleLayers = [];
+            var visibleLayers = [];
+            if (layer) { //We know we're in the National Level Data w/ the exception of Data Completeness
+                var dynamicLayer = brApp.map.getLayer('nationalLevel');
 
-            if (keys.length === 0) {
-                visibleLayers.push(-1);
+                switch (layer) {
+                    case ".1": //Community Land Tenure
+                        visibleLayers = [0];
+                        break;
+                    case ".2": //Indigenous Land Tenure
+                        visibleLayers = [1];
+                        break;
+                    case ".3": //Percent of Community and Indigenous
+                        keys[0] += 2; //--> To get around the two Land Tenure Security Layers
+                        visibleLayers = keys;
+                        break;
+                }
+
             } else {
-                arrayUtils.forEach(keys, function(key) {
-                    visibleLayers = visibleLayers.concat(MapConfig.layerMapping[key]);
-                });
-            }
-            console.log(visibleLayers);
-            if (document.getElementById('data-complete-checkbox').checked === true) {
-                visibleLayers.push(11);
+                var dynamicLayer = brApp.map.getLayer('indigenousLands');
+
+                if (keys.length === 0) {
+                    visibleLayers.push(-1);
+                } else {
+                    arrayUtils.forEach(keys, function(key) {
+                        visibleLayers = visibleLayers.concat(MapConfig.layerMapping[key]);
+                    });
+                }
+                console.log(visibleLayers);
+                if (document.getElementById('data-complete-checkbox').checked === true) {
+                    visibleLayers.push(5);
+                }
+
             }
             dynamicLayer.setVisibleLayers(visibleLayers);
+
 
             topic.publish('refresh-legend');
         },
