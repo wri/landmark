0f45009c9e99ace76d0fb0841da5ef783ac5af20
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-10-30 16:31:56 -0400

    incrememnted version number

--- src/js/dojoBootstrap.js
+++ src/js/dojoBootstrap.js
@@ -4,7 +4,7 @@
     var base = location.pathname.replace(/\/[^/]+$/, ''),
         isWordpress = location.hostname.search('landmark.blueraster.io') > -1,
         base = isWordpress ? base.replace('map', 'map-app') : base,
-        appVersion = '1.1.0',
+        appVersion = '1.1.1',
         esriVersion = '3.13',
         js = [
             '//js.arcgis.com/' + esriVersion + '/init.js'
